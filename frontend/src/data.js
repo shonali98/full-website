e5f78bc54a1e2b8c4f12f9bcbebdc9cb3a4422c1
shonali98 <shonalinethmika998@gmail.com>
2022-05-08 22:40:00 +0530

    2nd commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+export default{
+    products:[
+        {
+            _id:'1',
+            name:'Full Zip Running Shirts',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:70,
+            brand: 'Nike',
+            rating:4.5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+        {
+            _id:'2',
+            name:'Full Zip  Shirts',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:60,
+            brand:'Nike',
+            rating:4.5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+        {
+            _id:'3',
+            name:'Full Zip Running Shirts',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:40,
+            brand:'CK',
+            rating:5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+        {
+            _id:'4',
+            name:'Full Zip Running Shirts',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:60,
+            brand:'Hiskywin',
+            rating:4.5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+        {
+            _id:'5',
+            name:'Full Zip Running Shirts',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:60,
+            brand:'Hiskywin',
+            rating:4.5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+        {
+            _id:'6',
+            name:'Full Zip Running Shirts',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:60,
+            brand:'Hiskywin',
+            rating:4.5,
+            numRevies:10,
+            countInStock:6,
+
+        },
+
+    ],
+};
---
+++ frontend/src/screens/Error404Screen.js
@@ -0,0 +1,8 @@
+const Error404Screen ={
+    render:() =>{
+        return `<div>Page Not Found !</div>`;
+    },
+
+};
+
+export default Error404Screen;
